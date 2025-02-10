5388c1d8a57c87a294a56c98b14c12f80bb8750c
dimas1984 <dimas.w@polinema.ac.id>
2025-02-10 09:39:03 +0700

    spwaner mummy

---
+++ Assets/Mummy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mummy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public float speed = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = Camera.main.transform.position;
+
+        agent.SetDestination(targetPosition);
+        agent.speed = speed;
+    }
+}
